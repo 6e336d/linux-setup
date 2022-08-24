87aa1f5ef6f438292d85c38dca84d0f9d7f8d273
6e336d <79213211+6e336d@users.noreply.github.com>
2022-08-24 23:21:19 +0100

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -8,19 +8,6 @@ cd /root
 wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
 zsh install.sh
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-n
-n
-n
-1
-1
-3
-2
-3
-1
-2
-1
-n
-1
 rm .zshrc
 wget https://raw.githubusercontent.com/6e336d/linux-setup/main/zshrc
 mv zshrc .zshrc
