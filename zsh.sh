0276739b90e934c9481d1848b25db46580d6b081
6e336d <79213211+6e336d@users.noreply.github.com>
2022-08-24 23:05:50 +0100

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -3,7 +3,7 @@ apt-get -y upgrade
 apt -y install wget
 apt -y install zsh
 apt -y install git
-chsh root -s /bin/zsh
+chsh -y root -s /bin/zsh
 wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
 zsh install.sh
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
