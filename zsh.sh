93c91120533e02aa1b20178042fa7c88b7de51cd
6e336d <79213211+6e336d@users.noreply.github.com>
2022-08-24 22:42:12 +0100

    Create zsh.sh

---
+++ zsh.sh
@@ -0,0 +1,13 @@
+apt-get update
+apt-get upgrade
+apt -y install wget
+apt -y install zsh
+chsh root -s /bin/zsh
+wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+zsh install.sh
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+rm .zshrc
+wget https://raw.githubusercontent.com/6e336d/linux-setup/main/zshrc
+mv zshrc .zshrc
+zsh
+rm install.sh
